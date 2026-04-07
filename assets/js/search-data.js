a86041f3250c24acff6111c9ef57319ab5f5b49d
JyMiao122 <jm166@rice.edu>
2026-04-07 22:15:31 +0000

    Deploying to gh-pages from @ JyMiao122/JyMiao122.github.io@946a06b02eaf45d70db80f17bfac6a8eb93f20b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "people",
-          description: "About me",
+          description: "members of the lab or group",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
