2e7501f3d7321819a5b36cedc70af249dc653996
JyMiao122 <jm166@rice.edu>
2026-04-07 21:38:43 +0000

    Deploying to gh-pages from @ JyMiao122/JyMiao122.github.io@78ed1e152678a23b77334aa3f5bc20021e32c391 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-multiview-linear-array-pact",
+          title: 'Multiview Linear-Array PACT',
+          description: "Improving 3D photoacoustic imaging with multiview acquisition and reconstruction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
