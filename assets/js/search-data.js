c47465d22402226f80b6239364da4248e71c9683
JyMiao122 <jm166@rice.edu>
2026-04-08 17:50:16 +0000

    Deploying to gh-pages from @ JyMiao122/JyMiao122.github.io@3ca70442da362c63dac190cdf7f3cf68ccb468cb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "About me",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
